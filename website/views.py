20ef64096d6494c794acbb4c98ae98f28c069df6
Jahnavi <jahnaviraman186@gmail.com>
2024-02-03 22:47:13 -0500

    UI changes

--- website/views.py
+++ website/views.py
@@ -7,8 +7,8 @@ weather_dashboard = Blueprint('weather_dashboard', __name__)
 API_KEY = '4c1d64bec949b1fedecc27ffe4970435'
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
-def index():
-    return render_template('index.html')
+# def index():
+#     return render_template('index.html')
 
 @weather_dashboard.route('/', methods=['GET', 'POST'])
 def get_weather():
@@ -20,11 +20,12 @@ def get_weather():
             weather_data = response.json()
 
             if response.status_code == 200:
-                print(weather_data)
+                # print(weather_data)
                 temperature = weather_data['main']['temp']
                 icon = weather_data['weather'][0]['icon']
                 description = weather_data['weather'][0]['description']
-                return render_template('dashboard.html', city=city, temperature=temperature, description=description)
+                print(icon)
+                return render_template('dashboard.html', city=city, temperature=temperature, description=description, icon=icon)
             else:
                 error_message = f"Error: {weather_data['message']}"
                 return render_template('dashboard.html', error_message=error_message, city=city)
