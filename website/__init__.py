87cde0e042fabca626cc0582041c548bb3c94f97
Janhavi-23 <janhavip1231@gmail.com>
2024-02-04 01:54:41 -0500

    Crop statistics

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sjdkfbnhljsd sfdhjb'
+    app.config['SECRET_KEY'] = 'sjdkfbnhljsdsfdhjb'
     from .views import views
     app.register_blueprint(views, url_prefix="/")
 
